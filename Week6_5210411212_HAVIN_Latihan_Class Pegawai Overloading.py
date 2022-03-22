9e9323cb856789f77cc3762b3a269397b99a93d0
Havinoia <96777626+Havinoia@users.noreply.github.com>
2022-03-22 13:08:21 +0700

    Add files via upload

---
+++ Week6_5210411212_HAVIN_Latihan_Class Pegawai Overloading.py
@@ -0,0 +1,39 @@
+#5210411212
+#HAVIN NEO DIMAS NUGRAHA
+
+#Implementasi Overloading
+class Pegawai:
+    jumlah = 0
+    
+    def __init__(self, nama, gaji):
+        self.nama = nama
+        self.gaji = gaji
+        Pegawai.jumlah += 1
+        
+    def tampilJumlah(self):
+        print("Total pegawai", Pegawai.jumlah)
+        
+    def tampilPegawai(self):
+        print("Nama:", self.nama, ", gaji:", self.gaji)
+       
+    #Method Overloading
+    def tunjangan(self, istri=None, anak=None):
+        if anak != None and istri != None:
+            total = anak+istri
+            print("Tunjangan anak + istri =", total)
+        else:
+            total = istri
+            print("Tunjangan istri =", total)
+
+#memanggil kelas
+peg1 = Pegawai("Eren", 2000)
+peg2 = Pegawai("Luffy", 6000)
+peg1.tampilPegawai()
+peg2.tampilPegawai()
+peg2.tunjangan(2500,2000)  #overloading
+peg2.tunjangan(2500)       #overloading
+
+print("Total Pegawai %d" % Pegawai.jumlah)
+rataGaji=(peg1.gaji + peg2.gaji)/Pegawai.jumlah
+print("Rata-rata gaji = "+str(rataGaji))
+
---
+++ Week6_5210411212_HAVIN_Tugas_Class ComputerPart Overloading.py
@@ -0,0 +1,87 @@
+#5210411212
+#HAVIN NEO DIMAS NUGRAHA
+
+class ComputerPart:
+    def __init__(self, pabrikan, nama, harga) :
+        self.pabrikan = pabrikan
+        self.nama = nama    
+        self.harga = harga
+
+class Processor(ComputerPart) :
+    def __init__(self, pabrikan, nama, harga, core, speed) :
+        self.pabrikan = pabrikan
+        self.nama = nama 
+        self.harga = harga
+        self.core = core
+        self.speed = speed
+        
+    def Tampil(self) :  
+        print(f"{self.pabrikan} membuat seri produk yaitu {self.nama}")
+        print("Dengan Spesifikasi Berikut:")
+        print(f"Jumlah Core = {self.core}")
+        print(f"Boost Clock: {self.speed}")
+        print(f"Harga : {harga_brg(self.harga)}")
+
+class RandomAccessMemory(ComputerPart) :
+    def __init__(self, pabrikan, nama, harga) :
+        self.pabrikan = pabrikan
+        self.nama = nama 
+        self.harga = harga
+
+    def Tampil(self, kapasitas) :  
+        print(f"{self.pabrikan} membuat seri produk yaitu {self.nama}")
+        print("Dengan Spesifikasi Berikut:")
+        print(f"Kapasitas : {kapasitas}")
+        print(f"Harga : {harga_brg(self.harga)}")
+
+class HardDiskSATA(ComputerPart) :
+    def __init__(self, pabrikan, nama, harga) :
+        self.pabrikan = pabrikan
+        self.nama = nama 
+        self.harga = harga
+    
+    def Tampil(self, kapasitas, rpm) :
+        print(f"{self.pabrikan} membuat seri produk yaitu {self.nama}")
+        print("Dengan Spesifikasi Berikut:")
+        print(f"Kapasitas : {kapasitas}")
+        print(f"RPM : {rpm}")
+        print(f"Harga : {harga_brg(self.harga)}")
+       
+class GraphicsCard(ComputerPart) :
+    def __init__(self, pabrikan, nama, harga, MHz) :
+        self.pabrikan = pabrikan
+        self.nama = nama 
+        self.harga = harga
+        self.MHz = MHz
+    
+    def Tampil(self, kapasitas) :
+        print(f"{self.pabrikan} membuat seri produk yaitu {self.nama}")
+        print("Dengan Spesifikasi Berikut:")
+        print(f"Kapasitas : {kapasitas}")
+        print(f"MHz : {self.MHz}")
+        print(f"Harga : {harga_brg(self.harga)}")
+
+def harga_brg(harga) :
+    x = str(harga)
+    if len(x) <= 3 :
+        return "Rp." + x
+    else :  
+        a = x[:-3]
+        b = x[-3:]
+        return harga_brg(a) + '.' + b
+        
+
+proci = Processor('AMD', 'Ryzen™ 7 5800X', 6500000, 8, '4.7GHz')
+ram = RandomAccessMemory('Corsair', 'Corsair Vengeance DDR4 Dimm PC4-28800/3600MHz', 1500000)
+hdd = HardDiskSATA('WD Black', 'WD Black Caviar Dekstop HDD 3.5 inch', 2100000)
+vga = GraphicsCard('NVIDIA', 'NVIDIA RTX 3080 TI', 35000000, 1670)
+
+parts = [proci, ram, hdd, vga]   
+print("\n\t\t\t\tPart Komputer Overloading")
+print("---------------------------------------------------------------------------------\n")
+proci.Tampil()
+ram.Tampil("2x8GB")
+hdd.Tampil("1TB", 7200)
+vga.Tampil("12GB")
+print("---------------------------------------------------------------------------------")
+
